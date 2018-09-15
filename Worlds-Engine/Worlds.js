d78f7b6cdd2f3096c990b743919e25a1951186a6
ryan.cjw@gmail.com <ryan.cjw@gmail.com>
2018-09-15 09:13:40 -0700

    Worked on the socket interface

--- Worlds-Engine/Worlds.js
+++ Worlds-Engine/Worlds.js
@@ -4,7 +4,41 @@ Worlds.js
 */
 
 const Eos = require('eosjs');
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+server.on('error', (err) => {
+  console.log(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(3000);
+
+rl.question('What do you think of Node.js? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+
+  rl.close();
+};
+
+// server listening 0.0.0.0:41234
+
+/*
 const config = {
     expireInSeconds: 60,
     broadcast: true,
@@ -17,3 +51,4 @@ const config = {
 };
 
 const eos = Eos(config);
+*/
