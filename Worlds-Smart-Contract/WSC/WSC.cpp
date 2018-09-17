04257ca9304282f484b7396afe2c542de5a2dacc
Jonny Depp <ryan.cjw@gmail.com>
2018-09-17 16:56:31 -0700

    Compiled the paper. Implemeted some code for the WSC.

--- Worlds-Smart-Contract/WSC/WSC.cpp
+++ Worlds-Smart-Contract/WSC/WSC.cpp
@@ -2,14 +2,26 @@
 
 using namespace eosio;
 
-class hello : public eosio::contract {
-  public:
-      using contract::contract;
+struct item {
+	char ItemName[30];
+	char ItemClass[30];
+	account_name Owner ;
+	account_name PreviousOwner;
+	account_name OriginWorld;
+	time GenesisTime;
+	time TXtime;
+	float Stake;
+}
 
-      /// @abi action 
-      void hi( account_name user ) {
-         require_auth( user );
-				 print( "Hello, ", name{user} );
+class WSC: public eosio::contract {
+	public:
+		using contract::contract;
+		
+		checksum CreateItem(const account_name user ,const asset& quantity) {
+    	require_auth( user );
+			
+			item.GenesisTime = now()
+
       }
 };
 
