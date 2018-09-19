e5c56a0fbc6b318572a31266f96de322b462fb27
Jonny Depp <ryan.cjw@gmail.com>
2018-09-19 16:40:43 -0700

    Worked on the SC. Carving out the cpp file.

--- Worlds-Smart-Contract/WSC/WSC.cpp
+++ Worlds-Smart-Contract/WSC/WSC.cpp
@@ -1,33 +1,14 @@
 #include "WSC.hpp"
 
-using namespace eosio;
+// using namespace eosio;
+	
+void WSC::createitem(account_name Owner, struct item Fitem){
+// require_auth( Owner );
+/*Local Variables*/
+// checksum256 calc_hash;
 
+// item.GenesisTime = now();
 
-struct item {
-	char ItemName[30];
-	char ItemClass[30];
-	account_name Owner ;
-	account_name PreviousOwner;
-	account_name OriginWorld;
-	time GenesisTime;
-	time TXtime;
-	float Stake;
-};
+}
 
-
-class WSC: public eosio::contract {
-	public:
-		using contract::contract;
-		
-			void createitem(account_name user) {
-    	require_auth( user );
-			
-			/*Local Variables*/
-			checksum256 calc_hash;
-
-			// item.GenesisTime = now();
-
-      }
-};
-
-EOSIO_ABI( WSC, (createitem) )
+// EOSIO_ABI( WSC, (createitem) )
--- Worlds-Smart-Contract/WSC/WSC.hpp
+++ Worlds-Smart-Contract/WSC/WSC.hpp
@@ -6,3 +6,40 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/crypto.h>
 #include <eosiolib/types.h>
+
+using namespace eosio;
+
+class WSC: public contract{
+	public:
+		WSC(account_name self) : contract(self) {}
+		
+		/*This can just be a normal Struct*/
+		struct item {
+			char ItemName[30];
+			char ItemClass[30];
+			account_name Owner;
+			account_name PreviousOwner;
+			account_name OriginWorld;
+			time GenesisTime;
+			time TXtime;
+			float Stake;
+		};
+		
+		// @ abi action
+		void createitem(account_name Owner, struct item Fitem);
+
+	private:
+		// @abi table itemProof i64
+		struct itemProof{
+			account_name Owner;
+			checksum256  itemHash;
+			
+			account_name primary_key() const {return Owner;}
+			EOSLIB_SERIALIZE(itemProof, (Owner)(itemHash))
+		};
+
+		typedef eosio::multi_index<N(itemProofs), itemProof> itemProof_table;
+
+};
+
+EOSIO_ABI(WSC, (createitem))
