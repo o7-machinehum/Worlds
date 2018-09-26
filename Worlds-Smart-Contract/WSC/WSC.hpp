0c46a9299273caefa44cecef129264d63e0ef86d
Jonny Depp <ryan.cjw@gmail.com>
2018-09-26 12:38:52 -0700

    Moved WOR (tokens contract) into the WSC.

--- Worlds-Smart-Contract/WSC/WSC.hpp
+++ Worlds-Smart-Contract/WSC/WSC.hpp
@@ -18,7 +18,7 @@ using namespace eosio;
 using std::string;
 
 class WSC: public contract{
-  public:
+	public:
     WSC(account_name self) : contract(self) {}
     
     /*This can just be a normal Struct*/
@@ -33,14 +33,41 @@ class WSC: public contract{
       asset Stake;
     };
     
-    // @ abi action
+		[[eosio::action]]        
     void createitem(account_name Owner, string ItemName, string ItemClass, asset Stake);
-    // @ abi action
+		
+		[[eosio::action]]        
     void transferitem( account_name from, account_name to, item TxItem, checksum256 hash); 
+         
+		// WOR( account_name self ):contract(self){}
+
+		/** Token Actions **/
+		[[eosio::action]]
+		void create( account_name issuer,
+								asset        maximum_supply);
+
+		[[eosio::action]]        
+		void issue( account_name to, asset quantity, string memo );
+
+		[[eosio::action]]
+		void transfer( account_name from,
+									account_name to,
+									asset        quantity,
+									string       memo );
+
+		inline asset get_supply( symbol_name sym )const;
+
+		inline asset get_balance( account_name owner, symbol_name sym )const;
+
+		struct transfer_args {
+			account_name  from;
+			account_name  to;
+			asset         quantity;
+			string        memo;
+		};
 
   private:
-    // @abi table itemproofs i64
-    struct itemproof{
+    struct [[eosio::table]] itemproof{
       account_name Owner;
       checksum256  itemHash;
       
@@ -49,9 +76,43 @@ class WSC: public contract{
 			
 			EOSLIB_SERIALIZE(itemproof, (Owner)(itemHash))
     };
-
     typedef eosio::multi_index<N(itemproofs), itemproof> itemProof_table;
 
+		/** Token Private Members **/
+		struct [[eosio::table]] account {
+			asset    balance;
+
+			uint64_t primary_key()const { return balance.symbol.name(); }
+		};
+
+		struct [[eosio::table]] currency_stats {
+			asset          supply;
+			asset          max_supply;
+			account_name   issuer;
+
+			uint64_t primary_key()const { return supply.symbol.name(); }
+		};
+
+		typedef eosio::multi_index<N(accounts), account> accounts;
+		typedef eosio::multi_index<N(stat), currency_stats> stats;
+
+		void sub_balance( account_name owner, asset value );
+		void add_balance( account_name owner, asset value, account_name ram_payer );
+
 };
+
+asset WSC::get_supply( symbol_name sym )const
+   {
+      stats statstable( _self, sym );
+      const auto& st = statstable.get( sym );
+      return st.supply;
+   }
+
+   asset WSC::get_balance( account_name owner, symbol_name sym )const
+   {
+      accounts accountstable( _self, owner );
+      const auto& ac = accountstable.get( sym );
+      return ac.balance;
+   }
 
 EOSIO_ABI(WSC, (createitem)(transferitem))
