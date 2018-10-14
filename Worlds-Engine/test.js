3f4706e3fb053c20d0bbe3a315bba4b12ac6f479
machinehum <ryan.cjw@gmail.com>
2018-10-13 17:01:14 -0700

    Working on js. got actions working.

--- Worlds-Engine/test.js
+++ Worlds-Engine/test.js
@@ -1,39 +1,42 @@
 /* Item Structure
   - Just for testing..
 */
-Item.ItemName = 'Sword'
-Item.ItemClass = 'Weapon'
-Item.Owner = 'turnip'
-Item.PreviousOwner = ''
-Item.OriginWorld = ''
-Item.GenesisTime = ''
-Item.TXtime = ''
-Item.Stake =  ''
-
-function CreateItem(eos){
-  const actions = (await eos.getActions('turnip')).actions;
-  console.log(actions.map(a => a.action_trace));
+const item = {
+  ItemName: 'Sword',
+  ItemClass: 'Weapon',
+  Owner: 'turnip',
+  PreviousOwner:  '',
+  OriginWorld: '',
+  GenesisTime: '',
+  TXtime: '',
+  Stake: '',
 }
 
 Eos = require('eosjs')
 
-// Private key or keys (array) provided statically or by way of a function.
-// For multiple keys, the get_required_keys API is used (more on that below).
-keyProvider = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
-
 const config = {
-  chainId: null, // 32 byte (64 char) hex string
-  keyProvider: ['PrivateKeys...'], // WIF string or array of keys..
+  chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f', // 32 byte (64 char) hex string
+  keyProvider: ['5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'], 
   httpEndpoint: 'http://127.0.0.1:8888',
   expireInSeconds: 60,
   broadcast: true,
+  debug: true,
   verbose: false, // API activity
   sign: true
 };
 
+options = {
+  authorization: 'turnip@active',
+  broadcast: true,
+  sign: true
+}
+
 const eos = Eos(config);
 
-CreateItem(eos);
+eos.contract('wsc.code').then(wsccode => {wsccode.createitem("turnip", "Sword", "Weapon", "1.00 WOR", options)})
+// const actions = eos.getActions('turnip')).actions;
+
+// console.log(actions.map(a => a.action_trace));
 
 /*Get account actions*/
 
