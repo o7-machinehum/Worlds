f6e2fa0dfcc251e8fae7050adc3ae6589c75d745
Jonny Depp <ryan.cjw@gmail.com>
2018-10-17 17:29:57 -0700

    Upgrading to eosio.cdt 1.3 - I think this is going to be a bit painfull.

---
+++ Worlds-Smart-Contract/scripts/Wallet/createWallet.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cleos wallet create -n worlds --to-console
+cleos wallet import -n worlds --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
---
+++ Worlds-Smart-Contract/scripts/Wallet/unlockWallet.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cleos wallet unlock -n worlds --password PW5KCoeRPwx6LHcJTfAtctZQLiQiB9GGfyX5kUDHCZie9Bdx8s6qB 
