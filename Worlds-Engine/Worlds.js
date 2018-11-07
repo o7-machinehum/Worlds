05224e9b014b843a1a487e82e14d85988a717525
ryan.cjw@gmail.com <ryan.cjw@gmail.com>
2018-11-06 22:27:52 -0800

    Updated Worlds.js

--- Worlds-Engine/Worlds.js
+++ Worlds-Engine/Worlds.js
@@ -5,50 +5,9 @@ Worlds.js
 
 const Eos = require('eosjs');
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
-const readline = require('readline');
+const sock = require('./socket')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+port = 43252;
 
-server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`);
-  server.close();
-});
+sock.open(port) // Open socket
 
-server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
-
-server.on('listening', () => {
-  const address = server.address();
-  console.log(`server listening ${address.address}:${address.port}`);
-});
-
-server.bind(3000);
-
-rl.question('What do you think of Node.js? ', (answer) => {
-  // TODO: Log the answer in a database
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
-};
-
-// server listening 0.0.0.0:41234
-
-/*
-const config = {
-    expireInSeconds: 60,
-    broadcast: true,
-    debug: false,
-    sign: true,
-    // mainNet bp endpoint
-    httpEndpoint: 'https://api.eosnewyork.io',
-    // mainNet chainId
-    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-};
-
-const eos = Eos(config);
-*/
