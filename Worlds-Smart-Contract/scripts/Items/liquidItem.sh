3716d2a5e9f7ed846cde0359718956a40e46044a
Jonny Depp <ryan.cjw@gmail.com>
2018-11-06 21:19:30 -0800

    Added liquiditem script, it's not working yet.

---
+++ Worlds-Smart-Contract/scripts/Items/liquidItem.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Make these into vars in the future
+cleos push action wsc.code liquiditem '["owner":"turnip", "tx_item":{"Sword", "Weapon", "turnip", "0", "turnip", "0", "0", "1.00 WOR"}, "hash":"0x00"]' -p turnip@active
