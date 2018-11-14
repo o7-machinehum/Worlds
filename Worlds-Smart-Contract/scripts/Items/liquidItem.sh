cf1c89a43eb62d2a8a857df6cd39fd2bc58ffd40
machinehum <ryan.cjw@gmail.com>
2018-11-13 17:40:19 -0800

    Liquiditem function working and tested.

--- Worlds-Smart-Contract/scripts/Items/liquidItem.sh
+++ Worlds-Smart-Contract/scripts/Items/liquidItem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make these into vars in the future
-cleos push action wsc.code liquiditem '["owner":"turnip", "tx_item":{"Sword", "Weapon", "turnip", "0", "turnip", "0", "0", "1.00 WOR"}, "hash":"0x00"]' -p turnip@active
+cleos push action wsc.code liquiditem '{"owner":"turnip", "tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "PreviousOwner":"", "OriginWorld":"turnip", "GenesisTime":"1542159419", "TXtime":"0", "Stake":"1.00 WOR"}, "hash":"2a00cd4a42d7a0214a8d4de549e312c402cf410d9bc66430371e0229e6cb1c23"}' -p turnip@active
