f1d78309796760c7314736d788741b8b67db0353
machinehum <ryan.cjw@gmail.com>
2018-11-16 11:21:34 -0800

    Completely refactored the code, gentime is now kept on chain. Compacted some private functions into 1. createitem and liquiditem function both tested. Still need to test transfer item

--- Worlds-Smart-Contract/scripts/Items/liquidItem.sh
+++ Worlds-Smart-Contract/scripts/Items/liquidItem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make these into vars in the future
-cleos push action wsc.code liquiditem '{"owner":"turnip", "tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "PreviousOwner":"", "OriginWorld":"turnip", "GenesisTime":"1542159419", "TXtime":"0", "Stake":"1.00 WOR"}, "hash":"2a00cd4a42d7a0214a8d4de549e312c402cf410d9bc66430371e0229e6cb1c23"}' -p turnip@active
+cleos push action wsc.code liquiditem '{"owner":"turnip", "tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "OriginWorld":"turnip", "GenesisTime":"1542396039", "Stake":"1.00 WOR"}, "hash":"3037bf3bba0e1b35c51bd0c0c361deb578a856f615016520cb0542c255eeb8a2"}' -p turnip@active
