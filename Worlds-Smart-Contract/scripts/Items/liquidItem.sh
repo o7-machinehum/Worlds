cb29ee6aeaed2b37ca6a8cbc3b44392d88da98d1
Jonny Depp <ryan.cjw@gmail.com>
2018-11-21 18:16:36 -0800

    Reduced redundent arguments for some of the fuctions.

--- Worlds-Smart-Contract/scripts/Items/liquidItem.sh
+++ Worlds-Smart-Contract/scripts/Items/liquidItem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make these into vars in the future
-cleos push action wsc.code liquiditem '{"owner":"turnip", "tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "OriginWorld":"turnip", "GenesisTime":"1542406851", "Stake":"1.00 WOR"}, "hash":"add9de97f1b15dba7e957c4c2be83f46546de90b485f53c9f1e763f98dd4110f"}' -p turnip@active
+cleos push action wsc.code liquiditem '{"tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "OriginWorld":"turnip", "GenesisTime":"1542852009", "Stake":"1.00 WOR"}}' -p turnip@active
--- Worlds-Smart-Contract/scripts/Items/transferItem.sh
+++ Worlds-Smart-Contract/scripts/Items/transferItem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Make these into vars in the future
-cleos push action wsc.code transferitem '{"from":"turnip", "to":"turnip2", "tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "PreviousOwner":"", "OriginWorld":"turnip", "GenesisTime":"1542250757", "TXtime":"0", "Stake":"1.00 WOR"}, "hash":"981ffde650d54b5e73afdbb846265c6bd514007f28a4f8db6c69205520e64481"}' -p turnip@active
+cleos push action wsc.code transferitem '{"to":"turnip2", "tx_item":{"ItemName":"Sword", "ItemClass":"Weapon", "Owner":"turnip", "OriginWorld":"turnip", "GenesisTime":"1542852797", "Stake":"1.00 WOR"}}' -p turnip@active
