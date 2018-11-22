433c3c7f956ada0f4deca195d48f8d224631e9dc
Jonny Depp <ryan.cjw@gmail.com>
2018-11-21 18:45:14 -0800

    Forgot to add the deleteItem script

---
+++ Worlds-Smart-Contract/scripts/Items/deleteItem.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Make these into vars in the future
+cleos push action wsc.code deleteitem '{"owner":"turnip","hash":"1c6d935bca45b415a383547af17b87dfafd5e5191e059bb35b6fad23047b9a54"}' -p turnip@active
