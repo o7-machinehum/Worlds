42b440f58179bea187947d3af454aa4d2b3d866c
machinehum <ryan.cjw@gmail.com>
2018-12-08 16:11:27 -0800

    Implemented some non-working changes to the scripts

--- Worlds-Smart-Contract/scripts/Items/deleteItem.sh
+++ Worlds-Smart-Contract/scripts/Items/deleteItem.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# Make these into vars in the future
-cleos push action wsc.code deleteitem '{"owner":"turnip","hash":"fc557baaa410cea3af8579056164a498c74422e4e36d82fdc6006b55b8b91a36"}' -p turnip@active
+arg=''\''{"owner":"turnip","hash":"'$1'"}'\'''
+echo $arg
+
+# idk why this isn't working
+# cleos push action wsc.code deleteitem '{"owner":"turnip","hash":"{$1}"}' -p turnip@active
+cleos push action wsc.code deleteitem $arg turnip@active
