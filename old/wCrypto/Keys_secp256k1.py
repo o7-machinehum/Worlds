181ca763e174a41f0ce898984d8625eb7b943836
Ryan Walker <rwalker@MISTYWEST.local>
2019-01-25 14:55:58 -0800

    Removed some old files and added a licence

--- old/wCrypto/Keys_secp256k1.py
+++
@@ -1,8 +0,0 @@
-# https://github.com/warner/python-ecdsa
-
-from ecdsa import SigningKey
-sk = SigningKey.generate()
-vk = sk.get_verifying_key()
-
-open("keys/private.pem","w").write(sk.to_pem())
-open("keys/public.pem","w").write(vk.to_pem())
