1b4e12ea34a27bec504ae77b6311a1698700be05
machinehum <ryan.cjw@gmail.com>
2019-02-10 11:33:46 -0800

    Changed proof field and some output

--- Worlds-Engine/worldConnect.js
+++ Worlds-Engine/worldConnect.js
@@ -15,9 +15,9 @@ var world = {
     console.log(IP + Port + Name)
   
     // Write the player info down into a file
-    fs.writeFile('player/' + 'player.json', JSON.stringify({'Name': account.selectedName, 'proof': 'notMeow'}, null, 2), 'utf8', function(err){
+    fs.writeFile('player/' + 'player.json', JSON.stringify({'Name': account.selectedName, 'Proof': 'notMeow'}, null, 2), 'utf8', function(err){
       if(err) throw err;
-      console.log('Written Item to File!')
+      console.log('Written Proof to File!')
     });
     
     nodeCmd.get(exe, (err, data, stderr) => console.log(data)); // Run the client 
