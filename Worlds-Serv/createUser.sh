e65660eea1fe0adf594186eef769233017d57b10
machine-hum <ryan.cjw@gmail.com>
2019-05-28 16:42:35 -0700

    Added ftp files

---
+++ Worlds-Serv/createUser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+player=$1 # First CLI argument is the playername
+pw="test" # Change Later
+
+useradd $player
+echo -e "$pw\n$pw" | passwd $player
+mkdir -p /var/ftp/$player
+mkdir -p ~/players/$player/items
+mkdir -p ~/players/$player/exp
+
+mount --bind ~/players/$player /var/ftp/$player
+usermod -d /var/ftp/$player/ $player
